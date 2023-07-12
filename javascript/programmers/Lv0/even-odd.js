3d393b83b1cb3fe1eab8abf5c482019582c50877
sooyeon.jang <tn7278@naver.com>
2023-07-12 23:42:15 +0900

    study230712

--- javascript/programmers/Lv0/even-odd.js
+++ javascript/programmers/Lv0/even-odd.js
@@ -17,3 +17,13 @@ rl.on('line', function (line) {
         console.log(n + " is odd")
     }
 });
+
+// 
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// }).on('line', function (line) {
+//     const result = Number(line) % 2 ? 'odd' : 'even'
+//     console.log(line, 'is', result)
+// })
