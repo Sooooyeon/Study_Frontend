744df91cf2e11cd44867ac92afa0b6817d4961c5
sooyeon.jang <tn7278@naver.com>
2023-08-03 00:56:56 +0900

    로그인버튼 동적구현

--- challenges/login/js/script.js
+++ challenges/login/js/script.js
@@ -11,6 +11,17 @@ change.addEventListener('click', () => {
     }
 })
 
+const noticeId = document.querySelector('.btn-login');
+
+noticeId.addEventListener('click', () => {
+    // if문 필요 -> 아이디가 입력되지 않았을때
+    let notiid = document.getElementById('notice');
+    notiid.className += ' notiadd';
+
+    let notiidbox = document.getElementById('user-id');
+    notiidbox.className += ' inp-id-change';
+})
+
 
 // 쿼리셀렉터로 가상요소를 가져오지 못하는 이유는 html문서내에 그려지지 않아서이고, 
 // 가상요소 선택자가 돔 내부적으로 계산되어서 나와서 스타일을 적용할수가 없다?
