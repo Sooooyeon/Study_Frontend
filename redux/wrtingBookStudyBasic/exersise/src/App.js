f1f2411c622edd46dd7bf3d299a7b390d3acafcb
sooyeon.jang <tn7278@naver.com>
2023-09-14 03:23:53 +0900

    redux 기초

---
+++ redux/wrtingBookStudyBasic/exersise/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TextSize from './components/TextSize';
+
+function App() {
+  return (
+    <div className="App">
+      <TextSize />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux/wrtingBookStudyBasic/src/App.js
@@ -0,0 +1,9 @@
+import TextSize from './component/TextSize';
+
+function App() {
+  return (
+    <TextSize />
+  );
+}
+
+export default App;
